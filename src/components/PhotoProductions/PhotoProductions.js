0abfcb7e5231e93725feae9ef0fab8393ec11cab
pab-mich <pab.michelini@gmail.com>
2022-07-25 13:42:07 +0200

    production functionality done

---
+++ src/components/PhotoProductions/PhotoProductions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PhotoProductions = () => {
+  return <div>PhotoProductions</div>;
+};
+
+export default PhotoProductions;
--- src/components/Productions/Productions.js
+++ src/components/Productions/Productions.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
 import Banner from "../Banner/Banner";
-import "./Productions.css";
 import VideoProductions from "../../VideoProductions/VideoProductions";
+import PhotoProductions from "../PhotoProductions/PhotoProductions";
+import "./Productions.css";
 
 function Productions() {
+  const [clickedVideoProductions, setclickedVideoProductions] = useState("true");
+  const [clickedPhotoProductions, setclickedPhotoProductions] = useState("false");
+
+  const handleVideo = () => {
+    setclickedVideoProductions("true");
+    setclickedPhotoProductions("false");
+  };
+
+  const handlePhoto = () => {
+    setclickedVideoProductions("false");
+    setclickedPhotoProductions("true");
+  };
+
   return (
     <>
       <Banner />
       <div className='container-buttons'>
-        <a className='container-buttons-btn-gradient'>Phothos</a>
-        <a className='container-buttons-btn-gradient'>Videos</a>
+        <button onClick={handleVideo} className='container-buttons-btn-gradient'>
+          Videos
+        </button>
+        <button onClick={handlePhoto} className='container-buttons-btn-gradient'>
+          Phothos
+        </button>
       </div>
-      <div className='video-section-container'>
-        <VideoProductions />
-      </div>
+      {clickedVideoProductions === "true" ? (
+        <div className='video-section-container'>
+          <VideoProductions />
+        </div>
+      ) : (
+        ""
+      )}
+      {clickedPhotoProductions === "true" ? <PhotoProductions /> : ""}
     </>
   );
 }
