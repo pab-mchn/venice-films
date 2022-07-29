c461b224d877a3ca2f1a28646fdf9777552ab3ca
pab-mich <pab.michelini@gmail.com>
2022-07-29 19:19:11 +0200

    video modal changes

--- src/components/VideoProductions/VideoProductions.js
+++ src/components/VideoProductions/VideoProductions.js
@@ -44,7 +44,6 @@ const VideoProductions = () => {
                         width='900'
                         height='600'
                         src={videos.videoUrl}
-                        title='YouTube video player'
                         frameBorder='0'
                         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                         allowfullscreen></iframe>
